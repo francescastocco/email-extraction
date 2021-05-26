8d46b527ddc976df7afd01f0365c533cba0dbb72
Francesca Stocco <Francesca.Stocco@softwire.com>
2021-05-26 18:44:02 +0100

    add function to show top 10, frequency & group results

--- email-extraction.js
+++ email-extraction.js
@@ -37,9 +37,62 @@ function updateDomainCounter(dict, domainSet) {
     return dict;
 }
 
+// Function for choosing n highest attributes in an object.
+function pickHighest(obj, num = 1) {
+    const requiredObj = {};
+    if (num > Object.keys(obj).length) {
+        return false;
+    };
+    Object.keys(obj).sort((a, b) => obj[b] - obj[a]).forEach((key, ind) => {
+        if (ind < num) {
+            requiredObj[key] = obj[key];
+        }
+    });
+    return requiredObj;
+};
+
+// Function for returning attributes with more than n instances.
+const returnIfHigherThanN = (obj, num = 1,) => {
+    const requiredObj = {};
+    Object.keys(obj).forEach((key) => {
+        if (obj[key] > num) {
+            requiredObj[key] = obj[key];
+        }
+    });
+    return requiredObj;
+};
+
+// loop back through and count up different domains
+const updateGroupedDomainCounter = (dict, domainSet) => {
+    const groupedDomainSearch = new RegExp('[^\.]*');
+    for (let i = 0; i < flArray.length; i++) {
+        for (let item of domainSet) {
+            if (flArray[i].match('.*@' + item + '$')) {
+                const groupedKey = item.match(groupedDomainSearch);
+                dict[groupedKey]++;
+            }
+        }
+    }
+    return dict;
+}
+
+const getGroupedDomainSet = (domainSet) => {
+    const groupedDomainSearch = new RegExp('[^\.]*')
+    const groupedDomainList = [];
+    for (let item of domainSet) {
+        const groupedDomain = item.match(groupedDomainSearch);
+        groupedDomainList.push(groupedDomain);
+    }
+    return new Set(groupedDomainList);
+}
+
 const domainSet = getDomainSet();
 const domainCounter = getDomainCounter(domainSet);
 const result = updateDomainCounter(domainCounter, domainSet);
+
+const groupedDomainSet = getGroupedDomainSet(domainSet);
+const groupedDomainCounter = getDomainCounter(groupedDomainSet);
+const groupedResult = updateGroupedDomainCounter(groupedDomainCounter, domainSet)
 
 console.log('Please select the mode you want: \n1) All results \n2) Top 10 results \n3) More than n occurrences \n4) Grouped by domain');
 const mode = Number(readline.prompt());
@@ -60,4 +113,4 @@ if (mode === 1) {
 
 
 
-console.log(result);
+//console.log(result);
